812308c87fa7f44bdb644b088a16623b7d9f93b0
Andres Preciado <apreciado4@live.com>
2022-01-17 23:37:13 -0800

    Readme change

--- SimpleBanking.py
+++ SimpleBanking.py
@@ -5,7 +5,6 @@ Andres Preciado
 Using Sql Lite this Code system creates a Database of bank accounts
 where Accounts can be made, income added, balance kept, transfers,
 as well as closing the account.
-
 """
 import random
 import sqlite3
